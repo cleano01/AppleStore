aade4c294775e28a3fc89203b6e36f02a1362698
cleano01 <cleano.ferreira@dce.ufpb.br>
2020-02-16 11:34:42 -0300

    (genero) fazendo tratamento minimo nas funcoes

--- modules/genero/Genero.py
+++ modules/genero/Genero.py
@@ -1,24 +1,25 @@
 import pandas as pd
-'''Identificar quais são as 10 Aplicações do gênero Music
-e Book que possuem a maior quantidade de
-avaliações no arquivo csv apple_store.'''
+
 class Genero:
     def __init__(self):
         self.df = pd.read_csv("../../ultis/AppleStore.csv")
-        self.top_10_music = []
-        self.top_10_book = []
-
-    def top10_app_catg_Book(self):
-        lista_app_linha = self.df[self.df['prime_genre'] == 'Book'].sort_values('rating_count_tot', ascending=False).head(10)
-        #lista_app_linha['track_name']
-        print(lista_app_linha['track_name'].to_frame().values)
-
-    def top10_app_catg_Music(self):
-        lista_app_linha = self.df[self.df['prime_genre'] == 'Music'].sort_values('rating_count_tot', ascending=False).head(10)
-        
-        print(lista_app_linha)
-
+       
+    def top10_app_catg_book(self):
+        try:
+            lista_app_linha = self.df[self.df['prime_genre'] == 'Book'].sort_values('rating_count_tot', ascending=False).head(10)
+            return lista_app_linha['track_name'].to_frame().values
+        except:
+            print('Error : top10_app_catg_Book')
         
 
-g = Genero()
-g.top10_app_catg_Book()
+    def top10_app_catg_music(self):
+        try:
+            lista_app_linha = self.df[self.df['prime_genre'] == 'Music'].sort_values('rating_count_tot', ascending=False).head(10)
+            return lista_app_linha['track_name'].to_frame().values
+        except:
+            print('Error : top10_app_catg_Music')
+        
+
+
+        
+
